54c0f76a4523f8a41f3d5f7a4ec0cc0d7e3de5d3
hoylecake <thekidcorner@gmail.com>
2021-11-07 22:05:04 -0500

    rewrite ( again )
    bot actually has functionality now

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,37 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const { clientId, guildId, token } = require("./config.json");
 
-const commands = [];
+const commands = [
+  new SlashCommandBuilder()
+    .setName("mynuts")
+    .setDescription("Find the testicles .")
+    .addMentionableOption((option) =>
+      option
+        .setName("user")
+        .setDescription("User or role to check for testicles in")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder().setName("cool").setDescription("Cool test"),
+  new SlashCommandBuilder().setName("helpme").setDescription("Comamnds"),
+  new SlashCommandBuilder()
+    .setName("banme")
+    .setDescription("bans you from the server")
+    .addBooleanOption((option) =>
+      option.setName("kick").setDescription("False bans you, true kicks you")
+    ),
+  //   new SlashCommandBuilder()
+  //     .setName("ping")
+  //     .setDescription("pings everyone")
+  //     .addBooleanOption((option) =>
+  //       option.setName("here").setDescription("set to true to use @here")
+  //     ),
+].map((command) => command.toJSON());
 
 const rest = new REST({ version: "9" }).setToken(token);
 
 rest
   .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
+  .then(() => console.log("Good"))
   .catch(console.error);
--- index.js
+++ index.js
@@ -1,81 +1,35 @@
-// require the discord.js module
-const fs = require('fs');
-const { Client, Intents, Collection } = require('discord.js');
-require("dotenv").config(); // environment commands
-
-// config
+// Start farting
+const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+require("dotenv").config();
 
-// create a new Discord client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
-}
+// Wowza
+const client = new Client({ intents: [Intents.FLAGS.GUILDS], disableEveryone: false });
 const prefix = process.env.PREFIX;
 
-const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+// Die
 client.once('ready', () => {
 	console.log('A new age of darkness and despair befalls this server');
   console.log('Prefix is '+prefix);
 });
 
-// login to Discord with your app's token
-client.login(token);
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	
+  // console.log(interaction);
 
-
-// listen for messages and report them to the console
-client.on('message', message => {
-  console.log("DoDO Do Do DoDO")
-	const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
-	if (!prefixRegex.test(message.content)) return;
-
-	const [, matchedPrefix] = message.content.match(prefixRegex);
-	const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-    if (!client.commands.has(commandName)) return;
-
-    const command = client.commands.get(commandName);
-
-    try {
-	    command.execute(message, args);
-    } catch (error) {
-	    console.error(error);
-	    message.reply('something went wrong');
-    }
+  // if (interaction.commandName === "ping") {
+  //   const everyone = await interaction.guild.defaultRole; // dunno how to do it within the message
+  //   await interaction.reply(everyone+" hi there "+interaction.user.username+" wanted me to ping all of you");
+  // }
+  if (interaction.commandName === "mynuts") {
+    // console.log(interaction.options);
+    const rand = Math.round(Math.random());
+    const option = interaction.options._hoistedOptions[0]
+    await interaction.reply(`<@${option.name === "user" ? '!' : '&'}${option.value}> has ${rand ? "TESTICLES. Good Job. We're done here." : "NO TESTICLES! NO PENIS! NO BALLS! THEY ARE A  MOTHER    FUCKING KLUTZ!! Sorry I Dont make the rules around here."}`);
+  }
 });
 
-// client.on('message', async message => {
-// 	if (!message.content.startsWith(prefix) || message.author.bot) return;
+// Login to Discord with your client's token
+client.login(token);
 
-// 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-//     const commandName = args.shift().toLowerCase();
-
-//     const command = client.commands.get(commandName);
-
-//     if (!cooldowns.has(command.name)) {
-//         cooldowns.set(command.name, new Collection());
-//     }
-    
-//     const now = Date.now();
-//     const timestamps = cooldowns.get(command.name);
-//     const cooldownAmount = (command.cooldown || 3) * 1000;
-    
-//     if (timestamps.has(message.author.id)) {
-//         const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-    
-//         if (now < expirationTime) {
-//             const timeLeft = (expirationTime - now) / 1000;
-//             return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-//         }
-//     }
-// });
