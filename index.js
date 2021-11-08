432d384667f10693b5809393a54363deb1220989
hoylecake <thekidcorner@gmail.com>
2021-11-08 15:58:46 -0500

    webhook dumb shit and reverse optimization :))))) :-1:

--- index.js
+++ index.js
@@ -69,3 +69,8 @@ client.on('interactionCreate', async interaction => {
 client.login(token);
 
 const getIntOption = (interaction, id=0) => interaction.options._hoistedOptions[id];
+
+function isValidUrl(_string) {
+  const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+  return matchpattern.test(_string);
+}
