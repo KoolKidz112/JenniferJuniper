dc7b1547f8abbf33422561684dcca8ee32202b83
hoylecake <thekidcorner@gmail.com>
2021-11-08 15:38:04 -0500

    bunch of new stuff

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,11 +7,14 @@ const commands = [
   new SlashCommandBuilder()
     .setName("mynuts")
     .setDescription("Find the testicles .")
-    .addMentionableOption((option) =>
-      option
-        .setName("user")
-        .setDescription("User or role to check for testicles in")
-        .setRequired(true)
+    .addUserOption(
+      (
+        option // change to menitonable
+      ) =>
+        option
+          .setName("user")
+          .setDescription("User or role to check for testicles in")
+          .setRequired(true)
     ),
   new SlashCommandBuilder().setName("cool").setDescription("Cool test"),
   new SlashCommandBuilder().setName("helpme").setDescription("Comamnds"),
@@ -27,6 +30,38 @@ const commands = [
   //     .addBooleanOption((option) =>
   //       option.setName("here").setDescription("set to true to use @here")
   //     ),
+  new SlashCommandBuilder()
+    .setName("say")
+    .setDescription("make jenifer say ANYTHINg ....")
+    .addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("what you want jenifer junipir to say")
+        .setRequired(true)
+    ),
+  new SlashCommandBuilder()
+    .setName("webhooksend")
+    .setDescription("send data tnhrough the jeifer webhook")
+    .addStringOption((option) =>
+      option
+        .setName("content")
+        .setDescription(
+          "the text (can include mentions, links, videos, but not attachments and embeds)"
+        )
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("username")
+        .setDescription("the username")
+        .setRequired(false)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("avatar")
+        .setDescription("the url leading to the avatar image")
+        .setRequired(false)
+    ),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: "9" }).setToken(token);
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Start farting
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, WebhookClient } = require('discord.js');
 const { token } = require('./config.json');
 require("dotenv").config();
 
 // Wowza
 const client = new Client({ intents: [Intents.FLAGS.GUILDS], disableEveryone: false });
 const prefix = process.env.PREFIX;
+const webhookClient = new WebhookClient({ url: process.env.WEBHOOK_ID });
 
 // Die
 client.once('ready', () => {
@@ -23,13 +24,48 @@ client.on('interactionCreate', async interaction => {
   //   await interaction.reply(everyone+" hi there "+interaction.user.username+" wanted me to ping all of you");
   // }
   if (interaction.commandName === "mynuts") {
-    // console.log(interaction.options);
     const rand = Math.round(Math.random());
-    const option = interaction.options._hoistedOptions[0]
+    const option = getIntOption(interaction)
+    // console.log(option);
     await interaction.reply(`<@${option.name === "user" ? '!' : '&'}${option.value}> has ${rand ? "TESTICLES. Good Job. We're done here." : "NO TESTICLES! NO PENIS! NO BALLS! THEY ARE A  MOTHER    FUCKING KLUTZ!! Sorry I Dont make the rules around here."}`);
+    await interaction.followUp({"content":"sorry dont know how to mention roles","ephemeral":"true"})
+  } else if (interaction.commandName === "banme") {
+    const user = interaction.member;
+    const option = getIntOption(interaction)
+    console.log(option ? option.value : false);
+    user.send("Hi you are going to get kicked/baned if you wanna come back you can always rejoin with https://discord.gg/Zzw7rVhPXV (if yuo are Ip Baned ask the admins to get you back in)")
+    await interaction.reply("Goodbye");
+    if (option ? !option.value : false) { // WHY IS OPTION.VALUE NOT FALSE WHEN IT'S UNDEFINED YOU DAFT CUNT
+        user.ban({"reason":"Ip Baned by Jennifer Juniper (/banme)"}).catch(error => {
+          if (error.code === 50013) { interaction.followUp("Sory I don have permison to do this (Check to see if you are higher rank than me in the server becuz you cannot run this on owners)") }
+        });
+    } else {
+        user.kick("Kicked by Jennifer Juniper").catch(error => {
+          if (error.code === 50013) { interaction.followUp("Sory I don have permison to do this (Check to see if you are higher rank than me in the server becuz you cannot run this on owners)") }
+        });
+    }
+  } else if (interaction.commandName === "helpme") {
+    interaction.reply({"content":"Jennifer Juniper is right around the corner. And with that, sus. This BOT Runs OFF OF Slash Comansd. Just Press / on yor keyboard and all the commands along with descriptions Will Display.","ephemeral":"true","files":["https://media.discordapp.net/attachments/862154953295396884/906768994999107625/bunny.gif"]})
+  } else if (interaction.commandName === "say") {
+    const text = getIntOption(interaction);
+    console.log(text.value);
+    await interaction.reply(text.value);
+  } else if (interaction.commandName === "webhooksend") {
+    const cont = getIntOption(interaction,0).value;
+    const usern = getIntOption(interaction,1) ? getIntOption(interaction,1).value : "Jennifer Juniper";
+    const avatar = getIntOption(interaction, 2) ? (getIntOption(interaction, 2).value) : "https://images-ext-1.discordapp.net/external/A2wjggCFcu0g2vBts4wqO71sJ37xB43smDdDQE5GLHQ/https/media.discordapp.net/attachments/906768055311401013/907354948415651920/bunny.gif?width=94&height=94";
+    // console.log(cont,usern,avatar)
+    console.log(interaction.options._hoistedOptions)
+    await webhookClient.send({
+      content: cont,
+      username: usern,
+      avatarURL: avatar,
+      // embeds: [embed], TODO Add later
+    }).then(interaction.reply({"content":"successfuly sent your message","ephemeral":"true"}))
   }
 });
 
 // Login to Discord with your client's token
 client.login(token);
 
+const getIntOption = (interaction, id=0) => interaction.options._hoistedOptions[id];
